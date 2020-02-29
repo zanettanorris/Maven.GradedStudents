0a735c52cf41259700886d5ca671cba3c3028811
Zanetta Norris <znorris@zipcodes-MBP-2.localdomain>
2020-02-29 15:59:49 -0500

    Added code to Student and Student Test. Tests pass.

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,62 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+    String firstName;
+    String lastName;
+    ArrayList<Double> examScores;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+//    public ArrayList<Double> getExamScores() {
+//        return examScores;
+//    }
+
+//    public void setExamScores(ArrayList<Double> examScores) {
+//        this.examScores = examScores;
+//    }
+
+    public Student(String firstName, String lastName, ArrayList<Double> examScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = examScores;
+
+    }
+
+    public Student(String firstName, String lastName, Double[] testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList<>(Arrays.asList(testScores));
+    }
+
+    public Integer getNumberOfExamsTaken() {
+        return examScores.size();
+    }
+
+    public String getExamScores(){
+        String output = "Exam Scores:\n";
+        for (int i = 0; i < examScores.size(); i++){
+            output += "Exam " + (i+1) + " -> " + Math.round(examScores.get(i));
+            if ( i != examScores.size()-1){
+                output +=  "\n";
+            }
+        }
+        return output;
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,44 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class StudentTest {
+
+   String firstName;
+   String lastName;
+   ArrayList<Double> examScores;
+   Student student;
+
+   @Before
+   public void init() {
+       firstName = "Jimmy";
+       lastName = "Choo";
+       examScores = new ArrayList<>(Arrays.asList(100.0, 90.0, 95.0, 85.0));
+       student = new Student(firstName, lastName, examScores);
+   }
+
+    @Test
+    public void Student1Test(){
+        Assert.assertEquals("Jimmy", student.getFirstName());
+        Assert.assertEquals("Choo", student.getLastName());
+    }
+
+    @Test
+    public void getExamScoresTest(){
+       String expected = "Exam Scores:\n" +
+               "Exam 1 -> 100\n" +
+               "Exam 2 -> 90\n" +
+               "Exam 3 -> 95\n" +
+               "Exam 4 -> 85";
+       String actual = student.getExamScores();
+       Assert.assertEquals(expected, actual);
+    }
+
+
 
 }
